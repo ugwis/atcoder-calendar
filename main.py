fae364aed46de584deac826b0682d48d25136b86
Kitamura Yuta <ugwis@icloud.com>
2017-02-24 03:35:23 +0900

    タイムゾーン差を調節

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import datetime
 from datetime import datetime as dt
+import cal
+import re
 
 check_url = "https://atcoder.jp/contest"
+
+def regex(r,text):
+    rec = re.compile(r)
+    match = rec.search(text)
+    if match is None:
+        return None
+    return match.group(1)
 
 r = requests.get(check_url)
 soup = BeautifulSoup(r.text.encode(r.encoding),"html.parser")
@@ -16,10 +25,11 @@ for contest in contests:
     if len(tds) < 3:
         continue
     date = dt.strptime(tds[0].a.renderContents(), "%Y/%m/%d %H:%M")
+    
+    print(tds[1].a.get("href"))
+    id = regex("https://(\w*).contest.atcoder.jp",tds[1].a.get('href'))
     name = tds[1].a.renderContents()
     time = tds[2].renderContents().split(":")
-    start = date
-    end = date + datetime.timedelta(hours=int(time[0]), minutes=int(time[1]))
-    print(name)
-    print(start)
-    print(end)
+    start = date - datetime.timedelta(hours=9)
+    end = date + datetime.timedelta(hours=int(time[0]), minutes=int(time[1])) - datetime.timedelta(hours=9)
+    cal.add_event(id,name,start,end)
